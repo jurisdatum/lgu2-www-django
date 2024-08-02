90032f6df9c72b6dc9429b40393d5d82d1898860
Jim Mangiafico <jim@jurisdatum.com>
2024-08-02 13:54:07 -0400

    static browse page

--- urls.py
+++ urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('hello', views.hello, name='hello'),
+    path('ukpga', views.browse, name='ukpga'),
 ]
--- views.py
+++ views.py
@@ -1,9 +1,15 @@
 
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.template import loader
 
 def index(request):
-    return redirect('hello')
+    return redirect('ukpga')
 
 def hello(request):
     return HttpResponse("Hello world!")
+
+def browse(request):
+    template = loader.get_template('browse.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
