14a82b59bec3653480bc3ef218502f85494d00b3
Jim Mangiafico <jim@jurisdatum.com>
2024-08-16 19:53:36 -0400

    unique message names

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class Message(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     text = models.TextField()
 
     def __str__(self):
