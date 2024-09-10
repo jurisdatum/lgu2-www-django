2e3547e882edb83419e70ca767715df78284d9a9
Jim Mangiafico <jim@jurisdatum.com>
2024-09-10 13:27:14 -0400

    refactor views

--- urls.py
+++ urls.py
@@ -1,36 +1,40 @@
 
-from django.contrib import admin
-from django.urls import path, re_path
 from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import path, re_path
 
-from . import views
+from .views.browse import browse
+from .views.document import document, document_clml, document_akn
+from .views.metadata import metadata, combined
 
 urlpatterns = i18n_patterns(
 
-    path('', views.index, name='home'), prefix_default_language=False
+    path('', lambda r: redirect('browse'), name='home'), prefix_default_language=False
 )
 
 urlpatterns += [
 
     path('admin/', admin.site.urls),
-    path('hello', views.hello, name='hello'),
+    path('hello', lambda r: HttpResponse("Hello world!"), name='hello'),
 ]
 
 urlpatterns += i18n_patterns(
 
-    path('ukpga', views.browse, name='browse'),
-    path('ukpga/<int:year>', views.browse, name='browse-year'),
+    path('ukpga', browse, name='browse'),
+    path('ukpga/<int:year>', browse, name='browse-year'),
 
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)$', views.document, name='document'),
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/data.xml$', views.document_clml),
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/data.akn$', views.document_akn),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)$', document, name='document'),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/data.xml$', document_clml),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/data.akn$', document_akn),
 
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})$', views.document, name='document-version'),
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})/data.xml$', views.document_clml),
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})/data.akn$', views.document_akn),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})$', document, name='document-version'),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})/data.xml$', document_clml),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/(?P<version>enacted|\d{4}-\d{2}-\d{2})/data.akn$', document_akn),
 
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/metadata', views.metadata, name='metadata'),
-    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/combined', views.combined),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/metadata', metadata, name='metadata'),
+    re_path(r'^(?P<type>ukpga)/(?P<year>[0-9]{4})/(?P<number>[0-9]+)/combined', combined),
 
     prefix_default_language=False
 )
