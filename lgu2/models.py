2fef2da48e8509a00aba2945dbd04dcc747a25ec
Gurpreet Singh <gurpreet.singh@williamslea.com>
2025-02-05 16:34:02 +0530

    wagtail implementation with pages

--- lgu2/models.py
+++ lgu2/models.py
@@ -4,9 +4,10 @@ from wagtail.snippets.models import register_snippet
 from wagtail.admin.panels import FieldPanel
 from django.db import models
 from wagtail.fields import RichTextField  # Import RichTextField
+from wagtail.models import PreviewableMixin, RevisionMixin
 
 @register_snippet
-class Footer(models.Model):
+class Footer(RevisionMixin, PreviewableMixin, models.Model):
     """Model to store footer content."""    
     # Text fields for different languages
     copyright_statement_en = RichTextField(blank=True, help_text="Copyright statement in English")
@@ -19,9 +20,15 @@ class Footer(models.Model):
 
     def __str__(self):
         return 'Footer Content'
+    
+    def get_preview_template(self, request, preview_mode):
+        return f'browse/footer.html'
+    
+    def get_preview_context(self, request, preview_mode):
+        return {'footer': self}
 
 @register_snippet
-class AboutUsPage(models.Model):
+class AboutUsPage(RevisionMixin, PreviewableMixin, models.Model):
     main_heading = models.CharField(max_length=255, blank=True)
     subheading_1 = models.CharField(max_length=255, blank=True)
     introduction_subheading_1 = RichTextField(help_text="Introduction text for About this website section", blank=True)
@@ -34,10 +41,16 @@ class AboutUsPage(models.Model):
 
     def __str__(self):
         return self.main_heading
+    
+    def get_preview_template(self, request, preview_mode):
+        return f'browse/about_us.html'
+    
+    def get_preview_context(self, request, preview_mode):
+        return {'about_us_data': self}
 
 
 @register_snippet
-class AboutUsPageSubSection(models.Model):
+class AboutUsPageSubSection(RevisionMixin, PreviewableMixin, models.Model):
     subheading_2 = models.CharField(max_length=255)
     introduction_subheading_2 = RichTextField(help_text="Introduction text for About us", blank=True)
     
@@ -48,3 +61,35 @@ class AboutUsPageSubSection(models.Model):
 
     def __str__(self):
         return self.subheading_2
+    
+    def get_preview_template(self, request, preview_mode):
+        return f'browse/about_us.html'
+    
+    def get_preview_context(self, request, preview_mode):
+        context = {
+            'about_us_data': AboutUsPage.objects.all().first(),
+            'about_us_sub_sec': list(AboutUsPageSubSection.objects.all().exclude(id=self.id))  # Convert QuerySet to list
+        }
+        
+        # Now you can append the current object (`self`)
+        context['about_us_sub_sec'].append(self)
+        context['about_us_sub_sec'].sort(key=lambda x: x.id != self.id)  # This ensures 'self' comes first
+        
+        return context
+    
+
+# myapp/models.py
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from django.db import models
+
+class HelpPage(Page):
+    body = models.TextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
+
+    def get_preview_template(self, request, preview_mode):
+        return f'browse/help_guide.html'
+
