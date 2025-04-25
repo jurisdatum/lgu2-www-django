02d4752303627c0cb18a468832fbe3a632b63736
Gurpreet Singh <gurpreet.singh@williamslea.com>
2025-04-25 23:17:09 +0530

    wagtail code cleanup

--- lgu2/admin.py
+++ lgu2/admin.py
@@ -1,20 +1,4 @@
 
 from django.contrib import admin
-
 from .models import *
-
-# admin.site.register(Message)
-
-# @admin.register(DatasetCompleteness)
-# class DatasetAdmin(admin.ModelAdmin):
-#    list_display = [ 'type', 'cutoff' ]
-
-# from django.contrib import admin
-# from .models import Footer
-
-# class FooterAdmin(admin.ModelAdmin):
-#     list_display = ['copyright_statement_en', 'copyright_statement_cy']
-#     search_fields = ['copyright_statement_en', 'copyright_statement_cy']
-
-# admin.site.register(Footer, FooterAdmin)
 
