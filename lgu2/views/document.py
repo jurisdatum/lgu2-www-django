c07946b4c117a9498167157dd067b4ed1ebb1a27
Gurpreet Singh <gurpreet.singh@williamslea.com>
2026-03-17 01:21:28 +0530

    requested changes related or redirect and missing values in ukia function

--- lgu2/views/document.py
+++ lgu2/views/document.py
@@ -33,9 +33,13 @@ def document(request, type: str, year: str, number: str, version: Optional[str] 
         return HttpResponseNotFound(template.render({}, request))
 
     if type == 'ukia':
-        context = make_ukai_data(data, version, lang)
+        context = make_ukia_data(data, type, year, number, version, lang)
     else:
         context = make_document_context(data, type, year, number, version, lang)
+
+    # NEW: handle redirect
+    if isinstance(context, HttpResponse):
+        return context
 
     template = loader.get_template('new_theme/document/document.html')
     return HttpResponse(template.render(context, request))
@@ -115,24 +119,65 @@ def make_document_context(data, type, year, number, version, lang):
     return context
 
 
-def make_ukai_data(data, version, lang):
-    meta = data['meta']
+def make_ukia_data(data, type, year, number, version, lang):
+
+    meta = data['meta']    
+    meta['link'] = make_document_link(type, year, number, version, lang)
+
+    try:
+        timeline = make_timeline_data(meta, "document", lang) # timeline function needs version but there are not any in meta
+    except:
+        timeline = ""
+    
+    extent_label = ""
+    if meta.get('extent'):
+        extent_label = make_combined_extent_label(meta['extent']) # there is not extent present in meta
+        
     breadcrumbs = make_breadcrumbs(meta, version, lang)
+
+    explanatory_notes = []
+    other_associated_doc = []
+
+    if meta.get('associated') and len(meta['associated']) > 0:
+        for associated_documents in meta['associated']:
+            if associated_documents['type'] == 'Note':
+                explanatory_notes.append(associated_documents)
+            else:
+                other_associated_doc.append(associated_documents)
+
+    status = {} # making it blank because there is no title or effects in meta
+
+    meta['category'] = get_category(meta['shortType'])
+
+    # UKIA documents are PDF-only
+    pdf_only = True
+    pdf_link = meta['altFormats'][0]['url']
+    pdf_thumb = meta['altFormats'][0]['thumbnail']
+
     context = {
-        'meta': data['meta'],
+        'meta': meta,
         'view_date': meta.get('pointInTime') or timezone.localdate(),
-        'type_label_plural': get_type_label(data['meta']['longType']),
-        'timeline': '',
-        'extent_label': '',
+        'type_label_plural': get_type_label(meta['longType']),
+        'timeline': timeline,
+        'extent_label': extent_label,
         'breadcrumbs': breadcrumbs,
-        'explanatory_notes': '',
-        'other_associated_doc': '',
-        'status': '',
-        'article': '',
-        'pdf_only': True,
-        'pdf_link': '',
-        'pdf_thumb': '',
-        'altFormats' : data['meta']['altFormats']
+        'explanatory_notes': explanatory_notes,
+        'other_associated_doc': other_associated_doc,
+        'status': status,
+        'article': data.get('html'),
+        'links': {
+            'toc': make_contents_link(type, year, number, version, lang),
+            'content': make_fragment_link(type, year, number, 'introduction', version, lang),
+            'notes': '/',
+            'resources': '/',
+            'whole': None,
+            'body': make_fragment_link(type, year, number, 'body', version, lang),
+            'schedules': None if meta.get('schedules') is None else make_fragment_link(type, year, number, 'schedules', version, lang)
+        },
+        'pdf_only': pdf_only,
+        'pdf_link': pdf_link,
+        'pdf_thumb': pdf_thumb,
+        'altFormats': meta['altFormats']
     }
 
     return context
